fa9c167e380f5685ae44b2daba1d910991b00658
Cetin CAKIR <cetincakirtr@gmail.com>
2024-03-06 15:05:40 +0300

    feat: useState and useEffect

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,22 +1,44 @@
 import React from "react"
-import getRandomMeme from "./MemesData"
+
+function getRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
 
 function Meme() {
-    const [item, setItem] = React.useState(getRandomMeme)
-    const [topText, setTopText] = React.useState("")
-    const [bottomText, setBottomText] = React.useState("")
+    const [memesData, setMemesData] = React.useState([])
+    const [url, setUrl] = React.useState("")
+    const [texts, setTexts] = React.useState({ topText: "", bottomText: "" })
 
-    function handleItem(event) {
-        setItem(getRandomMeme)
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setMemesData(data.data.memes)
+            setUrl(getRandomItem(data.data.memes).url)
+
+        }
+        getMemes()
+
+        return () => {
+            // clean-up function
+        }
+    }, [])
+
+    function handleUrl(event) {
+        setUrl(getRandomItem(memesData).url)
         event.preventDefault()
     }
 
-    function handleTopText(event) {
-        setTopText(event.target.value);
-    };
-
-    function handleBottomText(event) {
-        setBottomText(event.target.value)
+    function handleTexts(event) {
+        const { name, value } = event.target
+        setTexts(prev => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
     }
 
 
@@ -24,15 +46,15 @@ function Meme() {
         <div className="meme">
             <form className="meme-form">
                 <div className="meme-form--inputs">
-                    <input type="text" id="meme-form--inputs-top" placeholder="Shut Up!" value={topText} onChange={handleTopText}></input>
-                    <input type="text" id="meme-form--inputs-bottom" placeholder="Take my money!" value={bottomText} onChange={handleBottomText}></input>
+                    <input type="text" className="meme-form--inputs-top" placeholder="Shut Up!" name="topText" value={texts.topText} onChange={handleTexts}></input>
+                    <input type="text" className="meme-form--inputs-bottom" placeholder="Take my money!" name="bottomText" value={texts.value} onChange={handleTexts}></input>
                 </div>
-                <button onClick={handleItem} type="submit" id="meme-form--submit">Get a new meme image</button>
+                <button onClick={handleUrl} type="submit" className="meme-form--submit">Get a new meme image</button>
             </form>
             <div className="image-container">
-                <img className="image-container--image" src={item.url} alt="meme image"></img>
-                <p className="image-container--top-text">{topText}</p>
-                <p className="image-container--bottom-text">{bottomText}</p>
+                <img className="image-container--image" src={url} alt="meme image"></img>
+                <p className="image-container--top-text">{texts.topText}</p>
+                <p className="image-container--bottom-text">{texts.bottomText}</p>
             </div>
         </div>
     )
